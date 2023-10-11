14b43899acc9946bba8a2dc895a9ed84aa749d26
alaglow <a.glowacka1295@gmail.com>
2023-10-11 14:30:46 +0200

    Fix style and cards

--- script.js
+++ script.js
@@ -1,34 +1,78 @@
 let playerPoints = 0;
+let computerPoints = 0;
 
-function drawCard(){
-return Math.floor(Math.random()*13)+1; 
+function drawCard() {
+    return Math.floor(Math.random() * 13) + 1;
 }
 
-function getCardPoints(card){
-    if (card === 1){
+function getCardPoints(card) {
+    if (card === 1) {
         return 11;
-    } else if (card === 10 || card === 11 || card === 12 || card === 13){
+    } else if (card === 10 || card === 11 || card === 12 || card === 13) {
         return 10;
-    } else{
+    } else {
         return card;
     }
 }
 
-function getCardImage(card){
-        return `images/${card}.jpg`
+function getCardImage(card) {
+    return `images/${card}.jpg`
 }
 
 function addUserPoints(card) {
-    let sumEl = document.getElementById("sum-el");
     playerPoints += getCardPoints(card);
-    sumEl.textContent = "Sum:" + " " + playerPoints;
 
+    document.getElementById("player-points-el").textContent = playerPoints;
 }
 
-function onHit(){
+function addComputerPoints(card) {
+    computerPoints += getCardPoints(card);
+
+    document.getElementById("computer-points-el").textContent = computerPoints;
+}
+
+function onHit() {
     let card = drawCard();
-    let cardImgEl = document.createElement('img')
-    cardImgEl.src = getCardImage(card)
-    document.getElementById('user-cards').appendChild(cardImgEl);
+    renderPlayerCard(card);
     addUserPoints(card);
+    evaluateScore();
+}
+
+function renderPlayerCard(card) {
+    let cardImgEl = document.createElement('img');
+    cardImgEl.src = getCardImage(card);
+    document.getElementById('user-cards').appendChild(cardImgEl);
+}
+
+function renderComputerCard(card) {
+    let cardImgEl = document.createElement('img');
+    cardImgEl.src = getCardImage(card);
+    document.getElementById('computer-cards').appendChild(cardImgEl);
+}
+
+function evaluateScore() {
+    if (playerPoints > 20) {
+        document.getElementById('hit-btn').disabled = true;
+    }
+
+    if (playerPoints > 21) {
+        document.getElementById("result-el").textContent = "You lost!"
+    } else if (playerPoints === 21) {
+        document.getElementById("result-el").textContent = "You win!"
+    }
+}
+
+function onStand() {
+    while (shouldComputerDraw()) {
+        let card = drawCard();
+        renderComputerCard(card);
+        addComputerPoints(card);
+    }
+}
+
+function shouldComputerDraw() {
+    if (computerPoints > 20 || computerPoints > playerPoints) {
+        return false;
+    }
+    return true;
 }
